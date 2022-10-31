ca2a24a8f06c44da49ad73805b81b3a11692462d
elthehablo <elthehablo3@gmail.com>
2022-10-31 13:40:00 +0100

    removed numpy and matplotlib dependencies

--- prog-1-okt-2022/ex2.py
+++ prog-1-okt-2022/ex2.py
@@ -1,8 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-
 def drawHangman(gamestate):
     """
     displays the current situation of hangman. gamestate is an array that shows how far the gallow is
